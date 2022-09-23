78b730612f3f156bdc26076dc86e04897b2e3eab
yohwanlee <dldyghks951@gmail.com>
2022-09-23 14:36:20 +0900

    math 연습문제 commit

--- src/Math/BOJ11653.java
+++ src/Math/BOJ11653.java
@@ -1,2 +1,22 @@
-package Math;public class BOJ11653 {
+package Math;
+
+import java.io.*;
+
+public class BOJ11653 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i < n + 1; i++) {
+
+            while (n % i == 0) {
+                bw.write(i + "");
+                bw.newLine();
+                n /= i;
+            }
+            if (n == 1) break;
+        }
+        bw.close();
+    }
 }
--- src/Math/BOJ1929.java
+++ src/Math/BOJ1929.java
@@ -1,2 +1,36 @@
-package Math;public class BOJ1929 {
+package Math;
+
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1929 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        boolean[] eratos = new boolean[m + 1]; //false면 소수
+        eratos[0] = true;
+        eratos[1] = true;
+
+        for (int i = 2; i <= m; i++) {
+            if (!eratos[i]) {
+                for (int j = i * 2; j < m + 1; j += i) {
+                    eratos[j] = true;
+                }
+            }
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = n; i < m + 1; i++) {
+            if (!eratos[i]){
+                bw.write(i + "");
+                bw.newLine();
+            }
+        }
+        bw.close();
+    }
 }
+
