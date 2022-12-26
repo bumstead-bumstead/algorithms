802f3c8f2f3e2e4d871006be355ba25bedf28749
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-12-26 23:36:11 +0900

    boj1644 풀이

--- src/hash/myHashSet.java
+++ src/hash/myHashSet.java
@@ -1,6 +1,8 @@
 package hash;
 
 
+import java.util.Arrays;
+
 //key가 String인 hash~
 public class myHashSet {
     int M = 1000003; //원소 수
@@ -9,8 +11,14 @@ public class myHashSet {
     int[] next = new int[MX]; //각 해시값의 다음 원소 idx
     int[] prev = new int[MX]; //각 해시값의 이전 idx
     int[] values = new int[MX];
-    int[] keys = new int[MX];
+    String[] keys = new String[MX];
     int unused = 0;
+
+    public myHashSet() {
+        Arrays.fill(head, -1);
+        Arrays.fill(next, -1);
+        Arrays.fill(prev, -1);
+    }
 
     private int hash(String key) {
         int hash = 0;
@@ -21,7 +29,31 @@ public class myHashSet {
     }
 
     public int find(String key) {
+        int h = hash(key);
+        if (head[h] == -1) return -1;
+
+        int tmp = head[h];
+        if (keys[tmp].equals(key)) return tmp;
+
+        while (next[tmp] != -1) {
+            if (keys[next[tmp]].equals(key)) {
+                return tmp;
+            }
+            tmp = next[tmp];
+        }
+
+        return -1;
+    }
+
+    public void insert(String key, int value) {
+        int idx = find(key);
         
+        //값이 존재할 때 대체
+        if (idx != -1) {
+            return;
+        }
+        
+        //없을 때 추가`
     }
 
 }
