6ff4acff7120565963e753c1c9671c6dd42666e8
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2023-01-12 01:56:36 +0900

    gitignore 다시 적용

--- src/binarySearchTree/test.java
+++ src/binarySearchTree/test.java
@@ -1,2 +1,13 @@
-package binarySearchTree;public class test {
+package binarySearchTree;
+
+import com.sun.source.util.Trees;
+
+import java.util.TreeSet;
+
+public class test {
+    public static void main(String[] args) {
+        TreeSet<Integer> a = new TreeSet<>();
+
+        System.out.println(a.higher(0));
+    }
 }
